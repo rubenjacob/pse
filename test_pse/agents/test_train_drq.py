920fc702ee046102cfdf9888f3b2a09993f8843c
ruben <uhgul@student.kit.edu>
2021-11-21 17:56:18 +0100

    add basic integration test

---
+++ test_pse/agents/test_train_drq.py
@@ -0,0 +1,9 @@
+import hydra
+
+from pse.agents.train_drq import Workspace
+
+
+@hydra.main(config_path="../../configs/test_config.yaml")
+def test_workspace_builds_and_forward_pass_works(cfg):
+    workspace = Workspace(cfg)
+    workspace.train()
