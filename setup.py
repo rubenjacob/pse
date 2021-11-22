3e07ac765acde40a24b67a062b620fd44d9cec19
ruben <uhgul@student.kit.edu>
2021-11-22 15:08:54 +0100

    switch to poetry

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="pse",
-    author="Ruben Jacob",
-    author_email="rubenjacob@outlook.com",
-    packages=find_packages(exclude=['test_pse'])
-    # install dependencies separately using conda
-)
