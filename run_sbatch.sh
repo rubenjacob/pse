bdef73aea020fc48acd7d38fad6f586df9b67831
ruben <uhgul@student.kit.edu>
2021-11-25 22:30:40 +0100

    use bash script instead of cw2

---
+++ run_sbatch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH -c 4
+#SBATCH -t 12:00:00
+#SBATCH --mem=20gb
+#SBATCH -p gpu_8
+#SBATCH -J  ruben_pse_baseline_experiment_0
+#SBATCH --gres=gpu:1
+
+#SBATCH -D /pfs/data5/home/kit/anthropomatik/ht9329/pse
+#SBATCH -o /pfs/data5/home/kit/anthropomatik/ht9329/pse/slurm_outputs/slurmlog/out_%A_%a.log
+#SBATCH -e /pfs/data5/home/kit/anthropomatik/ht9329/pse/slurm_outputs/slurmlog/err_%A_%a.log
+
+poetry run python3 pse/run.py
