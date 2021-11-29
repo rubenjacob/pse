3769e368f3997a848596743a198dd3add585f3de
ruben <uhgul@student.kit.edu>
2021-11-29 20:24:28 +0100

    fix debug config

--- pse/integration_test.py
+++ pse/integration_test.py
@@ -3,7 +3,7 @@ import hydra
 from pse.agents.train import Workspace
 
 
-@hydra.main(config_path="../configs", config_name="test_config.yaml")
+@hydra.main(config_path="../configs", config_name="debug_config.yaml")
 def check_workspace_builds_and_forward_pass_works(cfg):
     print(cfg)
     workspace = Workspace(cfg)
